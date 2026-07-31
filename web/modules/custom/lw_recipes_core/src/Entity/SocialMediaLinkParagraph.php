b3eae274598c6f96f2383ef2dc2a80fffd45e987
Lisa Williams <lisa@lisawilliams.net>
2026-07-30 20:23:42 -0400

    Code and templates for global footer

---
+++ web/modules/custom/lw_recipes_core/src/Entity/SocialMediaLinkParagraph.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\lw_recipes_core\Entity;
+use Drupal\Core\Entity\Attribute\Bundle;
+
+
+use Drupal\paragraphs\Entity\Paragraph;
+
+/**
+ * Bundle class for Recipe nodes
+ */
+
+#[Bundle(
+  entityType: 'paragraph',
+  bundle: 'social_media_link'
+)]
+class SocialMediaLinkParagraph extends Paragraph {
+
+  public function getSocialMediaIcon(): string|null {
+
+    $icon_map = [
+      'facebook' => 'fb.svg',
+      'instagram' => 'instagram.svg',
+      'linkedin' => 'linked-in.svg',
+      'youtube' => 'youtube.svg'
+    ];
+
+    if ($this->hasField('social_media_site') && !$this->get('social_media_site')->isEmpty()) {
+        $icon = $this->get('social_media_site')->value;
+        return \Drupal::service('extension.list.theme')->getPath('lw_recipes') . '/assets/img/' . $icon_map[$icon];
+    }
+
+    return null;
+  }
+
+  public function getUrl(): string|null {
+    if ($this->hasField('social_media_url') && !$this->get('social_media_url')->isEmpty()) {
+        return $this->get('social_media_url')->getValue()[0]['uri'];
+    }
+
+    return null;
+  }
+}
